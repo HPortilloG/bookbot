facf652cee8c2e03ca9f24601e8a0721fa47d6a3
Hugo <hugorpg015@gmail.com>
2023-04-09 23:49:14 -0300

    bookboot solution

---
+++ main.py
@@ -0,0 +1,37 @@
+def main():
+    book_path = "books/Frankeisten.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+
+    return len(words)
+
+def get_chars_dict(text):
+
+    chars = {}
+
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+
+    return chars
+
+
+def get_character_sums(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
